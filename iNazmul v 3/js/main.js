9d324d48bd17183de9bcf53fbe606625b6f7f212
Nazmul Hassan <Nazmul.naaz96@gmail.com>
2021-03-14 13:35:15 +0000

    update

--- iNazmul v 3/js/main.js
+++ iNazmul v 3/js/main.js
@@ -1,11 +1,11 @@
-var mybutton = document.getElementById("return-button");
+const mybutton = document.getElementById("return-button");
 
 window.onscroll = function () {
     scrollFunction()
 };
 
 function scrollFunction() {
-    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
         mybutton.style.display = "block";
     } else {
         mybutton.style.display = "none";
